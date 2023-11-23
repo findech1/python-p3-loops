1840a7d213cd7a0b2f7a49b2f47f997994066c71
Felix <findech6@gmail.com>
2023-11-23 22:41:42 +0300

    assignment

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,33 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+     print(counter)
+    counter -=1
+  
+print("Happy New Year!")
+
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    squared_lst = [x**2 for x in lst]
+    return squared_lst
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for num in range(1, 101):
+        print(fizzbuzz(num))
+
+def fizzbuzz(num):
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
+
+# Calling the function to print FizzBuzz sequence
+fizzbuzz_printer()
